e9fed93c12c012e79f75d7263f5a409dc3a2fb90
Dymey Chhun <dymeychhun12@gmail.com>
2025-10-05 20:39:39 +0700

    emove

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const axios = require("axios");
 const app = express();
 
-const port = process.env.PORT;
+// const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
@@ -64,6 +64,7 @@ app.post("/check-key", async (req, res) => {
   }
 });
 
-app.listen(port, () => {
-  console.log(`server is running on: http://localhost:${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`server is running on: http://localhost:${port}`);
+// });
+module.exports = app;
