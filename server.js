cdd5d8155a1b1410553f6e8321cdbda0e87cee5d
Dymey Chhun <dymeychhun12@gmail.com>
2025-10-05 21:26:48 +0700

    Add path

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 require("dotenv").config();
 const cors = require("cors");
 const axios = require("axios");
@@ -8,10 +9,10 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.set("view engine", "ejs");
-app.set("views", "./views");
+app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
   res.render("home", {
